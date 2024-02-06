e9c8d2febc926a7001890cb160ceeba6149a0d01
OsqY <oscarjosehpc@gmail.com>
2024-02-05 21:11:29 -0600

    Added clean buttons

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -18,15 +18,15 @@ const Editor = ({ entry }) => {
     { name: 'Mood', value: mood },
     { name: 'Negative', value: negative ? 'True' : 'False' },
   ]
-  // useAutosave({
-  //   data: value,
-  //   onSave: async (_value) => {
-  //     setIsLoading(true)
-  //     const updated = await updateEntry(entry.id, _value)
-  //     setAnalysis(updated.analysis)
-  //     setIsLoading(false)
-  //   },
-  // })
+  useAutosave({
+    data: value,
+    onSave: async (_value) => {
+      setIsLoading(true)
+      const updated = await updateEntry(entry.id, _value)
+      setAnalysis(updated.analysis)
+      setIsLoading(false)
+    },
+  })
 
   const handleDelete = async () => {
     const status = await deleteEntry(entry.id)
